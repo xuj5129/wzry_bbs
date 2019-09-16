f9ba8426e8a7f3c343c0406d257486d7a88c54aa
xuj <xuj5129@163.com>
2019-09-16 16:08:55 +0800

    许俊杰:Article相关类的创建

--- royal_service/src/main/java/com/bbs/service/ArticleService.java
+++ royal_service/src/main/java/com/bbs/service/ArticleService.java
@@ -2,7 +2,11 @@ package com.bbs.service;
 
 import com.bbs.domain.Article;
 
+import java.util.List;
+
 public interface ArticleService {
 
     void saveArticle(Article article);
+
+    List<Article> findAll();
 }
