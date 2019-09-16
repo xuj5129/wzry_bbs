7d44a427c6750e2d29cba38cddffd65e60039aca
xuj <xuj5129@163.com>
2019-09-16 15:39:41 +0800

    许俊杰:使包可见

---
+++ royal_service/src/main/java/com/bbs/service/impl/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.bbs.service.impl;
+
+import com.bbs.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+}
