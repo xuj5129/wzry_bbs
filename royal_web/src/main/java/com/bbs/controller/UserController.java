706a4f52f35bd4bb9dcb8e147d59ad2c919aa8f4
admin <admin@itcast.com>
2019-09-16 15:09:01 +0800

    添加jsp

--- royal_web/src/main/java/com/bbs/controller/UserController.java
+++ royal_web/src/main/java/com/bbs/controller/UserController.java
@@ -1,4 +1,13 @@
 package com.bbs.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/user")
 public class UserController {
+
+
+
 }
