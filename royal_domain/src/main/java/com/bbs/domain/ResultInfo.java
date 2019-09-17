8aa222000673144da238472098db6df3128a5545
admin <admin@itcast.com>
2019-09-17 11:57:07 +0800

    修改userInfo,添加resultInfo

---
+++ royal_domain/src/main/java/com/bbs/domain/ResultInfo.java
@@ -0,0 +1,31 @@
+package com.bbs.domain;
+
+public class ResultInfo {
+    private boolean success;
+    private String msg;
+    private Object object;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Object getObject() {
+        return object;
+    }
+
+    public void setObject(Object object) {
+        this.object = object;
+    }
+}
